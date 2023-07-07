599448b981fd107bae3e759af39a86cff3b851d1
xxjay <760409558@qq.com>
2023-07-07 14:21:14 +0800

    feat: 内存cell值转换RGBA颜色

--- ppu/color.go
+++ ppu/color.go
@@ -1,1 +1,24 @@
 package ppu
+
+type Color struct {
+	R uint8
+	G uint8
+	B uint8
+	A uint8
+}
+
+// getRGBAColor 将一个内存cell的值转换成RGBA颜色
+func getRGBAColor(cell byte) Color {
+	switch cell {
+	case 0:
+		return Color{0x0, 0x0, 0x0, 0xff}
+	case 1:
+		return Color{0xff, 0xff, 0xff, 0xff}
+	default:
+		return Color{0xff, 0xff, 0xff, 0x0}
+	}
+}
+
+func (c Color) Uint32() uint32 {
+	return uint32(c.R)<<16 | uint32(c.G)<<8 | uint32(c.B)
+}
--- ppu/ppu.go
+++ ppu/ppu.go
@@ -1,1 +1,18 @@
 package ppu
+
+// ReadAndUpdateScreen 从内存读取每个cell，并在frame中修改cell的值，如果有更新则通知给渲染器
+func ReadAndUpdateScreen(memory []byte, frame []byte) (updated bool) {
+	idx := 0
+	for _, cell := range memory {
+		color := getRGBAColor(cell)
+		r, g, b := color.R, color.G, color.B
+		if frame[idx] != r || frame[idx+1] != g || frame[idx+2] != b {
+			frame[idx] = r
+			frame[idx+1] = g
+			frame[idx+2] = b
+			updated = true
+		}
+		idx += 3
+	}
+	return
+}
