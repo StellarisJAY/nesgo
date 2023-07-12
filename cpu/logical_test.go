a6e22c6b7fd17af3e25bad29c4319edfbe91bf6a
xxjay <760409558@qq.com>
2023-07-12 10:48:58 +0800

    fix: cpu指令测试代码访问了ppu内存

--- cpu/logical_test.go
+++ cpu/logical_test.go
@@ -13,7 +13,7 @@ func TestLogicalAND(t *testing.T) {
 	}{
 		{"im", []byte{LDA_IM, 0b11011, AND_IM, 0b00001}, 1},
 		{"zp", []byte{LDA_IM, 0b11011, STA_ZP, 0x2, AND_ZP, 0x2}, 0b11011},
-		{"abs", []byte{LDA_IM, 0b11011, STA_ABS, 0x2, 0x30, AND_ABS, 0x2, 0x30}, 0b11011},
+		{"abs", []byte{LDA_IM, 0b11011, STA_ABS, 0x2, 0x1, AND_ABS, 0x2, 0x1}, 0b11011},
 	}
 
 	for _, c := range testCases {
@@ -34,7 +34,7 @@ func TestLogicalEOR(t *testing.T) {
 	}{
 		{"im", []byte{LDA_IM, 0b11011, EOR_IM, 0b00100}, 0b11111},
 		{"zp", []byte{LDA_IM, 0b11011, STA_ZP, 0x2, EOR_ZP, 0x2}, 0},
-		{"abs", []byte{LDA_IM, 0b11011, STA_ABS, 0x2, 0x30, EOR_ABS, 0x2, 0x30}, 0},
+		{"abs", []byte{LDA_IM, 0b11011, STA_ABS, 0x2, 0x1, EOR_ABS, 0x2, 0x1}, 0},
 	}
 
 	for _, c := range testCases {
@@ -55,7 +55,7 @@ func TestLogicalOR(t *testing.T) {
 	}{
 		{"im", []byte{LDA_IM, 0b11011, ORA_IM, 0b00100}, 0b11111},
 		{"zp", []byte{LDA_IM, 0b11011, LDX_IM, 0b00100, STX_ZP, 0x2, EOR_ZP, 0x2}, 0b11111},
-		{"abs", []byte{LDA_IM, 0b11011, LDX_IM, 0b00100, STX_ABS, 0x2, 0x30, EOR_ABS, 0x2, 0x30}, 0b11111},
+		{"abs", []byte{LDA_IM, 0b11011, LDX_IM, 0b00100, STX_ABS, 0x2, 0x1, EOR_ABS, 0x2, 0x1}, 0b11111},
 	}
 
 	for _, c := range testCases {
