ff64055b33b3a430dcc1c63f2ea1468cfb279247
xxjay <760409558@qq.com>
2023-08-01 17:07:42 +0800

    fix: switch bank错误

--- cartridge/mmc1.go
+++ cartridge/mmc1.go
@@ -3,8 +3,8 @@ package cartridge
 // Mapper1 支持切换chr和prg
 type Mapper1 struct {
 	raw      []byte
-	prgStart uint16
-	chrStart uint16
+	prgStart uint32
+	chrStart uint32
 
 	prgRAM      []byte
 	shiftReg    mapper1ShiftRegister
@@ -13,6 +13,7 @@ type Mapper1 struct {
 	prgROM      [2][]byte
 	chrBankRegs [2]byte
 	chrBanks    [2][]byte
+	chrRAM      bool
 }
 
 type mapper1ShiftRegister struct {
@@ -30,7 +31,20 @@ func NewMapper1(raw []byte) *Mapper1 {
 	if raw[6]&0b10 != 0 {
 		prgRAM = make([]byte, 0x1000)
 	}
-	_, prgStart, chrStart := splitPrgAndChr(raw)
+	_, prgStart, chrStart, chrRAM := splitPrgAndChr(raw)
+	var chrBanks [2][]byte
+	if chrRAM {
+		chrBanks = [2][]byte{
+			make([]byte, 0x1000),
+			make([]byte, 0x1000),
+		}
+	} else {
+		chrBanks = [2][]byte{
+			raw[chrStart : chrStart+0x1000],
+			raw[chrStart+0x1000 : chrStart+0x2000],
+		}
+	}
+
 	return &Mapper1{
 		prgRAM:   prgRAM,
 		raw:      raw,
@@ -40,10 +54,8 @@ func NewMapper1(raw []byte) *Mapper1 {
 			raw[prgStart : prgStart+0x4000],
 			raw[prgStart+0x4000 : prgStart+0x8000],
 		},
-		chrBanks: [2][]byte{
-			raw[chrStart : chrStart+0x1000],
-			raw[chrStart+0x1000 : chrStart+0x2000],
-		},
+		chrBanks: chrBanks,
+		chrRAM:   chrRAM,
 	}
 }
 
@@ -71,9 +83,9 @@ func (m *Mapper1) Write(addr uint16, val byte) {
 func (m *Mapper1) GetMirroring() byte {
 	switch m.ctrlReg & 0b11 {
 	case 0:
-		fallthrough
+		return OneScreenLow
 	case 1:
-		return OneScreen
+		return OneScreenHigh
 	case 2:
 		return Vertical
 	case 3:
@@ -87,15 +99,22 @@ func (m *Mapper1) GetChrBank(bank byte) []byte {
 	return m.chrBanks[bank]
 }
 
+func (m *Mapper1) WriteCHR(addr uint16, val byte) {
+	if m.chrRAM {
+		bank, off := addr/0x1000, addr%0x1000
+		m.chrBanks[bank][off] = val
+	}
+}
+
 func (m *Mapper1) writePrgRAM(addr uint16, val byte) {
 	if len(m.prgRAM) > 0 {
-		m.prgRAM[addr-0x6000] = val
+		m.prgRAM[(addr-0x6000)%0x1000] = val
 	}
 }
 
 func (m *Mapper1) readPrgRAM(addr uint16) byte {
 	if len(m.prgRAM) > 0 {
-		return m.prgRAM[addr-0x6000]
+		return m.prgRAM[(addr-0x6000)%0x1000]
 	}
 	return 0
 }
@@ -124,6 +143,7 @@ func (m *Mapper1) writeShiftRegister(addr uint16, val byte) {
 	// bit 7 set, clear shift register
 	if val&0x80 != 0 {
 		m.shiftReg.clear()
+		m.ctrlReg = m.ctrlReg | 0x0c
 	} else {
 		// 第五次写shiftReg，shiftReg值写到addr对应的内部地址，并重置shiftReg
 		if res, last := m.shiftReg.write(val); last {
@@ -150,8 +170,24 @@ func (sr *mapper1ShiftRegister) write(val byte) (byte, bool) {
 	}
 }
 
+// writeCtrl 修改ctrl寄存器，会导致chr和prg bank修改
 func (m *Mapper1) writeCtrl(val byte) {
 	m.ctrlReg = val
+	// 重新计算prg banks
+	m.writePrgBankReg(m.prgBankReg)
+	// 重新计算chr banks
+	mode := m.ctrlReg & (1 << ctrlRegChrBankMode)
+	if mode != 0 {
+		bank0 := m.chrStart + uint32(m.chrBankRegs[0])*4096
+		bank1 := m.chrStart + uint32(m.chrBankRegs[1])*4096
+		m.chrBanks[0] = m.raw[bank0 : bank0+4096]
+		m.chrBanks[1] = m.raw[bank1 : bank1+4096]
+	} else {
+		// 8KiB模式，切换两个chr banks, 忽略bit0
+		offset := m.chrStart + uint32(val&0xfe)*4096
+		m.chrBanks[0] = m.raw[offset : offset+4096]
+		m.chrBanks[1] = m.raw[offset+4096 : offset+8192]
+	}
 }
 
 func (m *Mapper1) writeChrBankReg(bank byte, val byte) {
@@ -159,11 +195,11 @@ func (m *Mapper1) writeChrBankReg(bank byte, val byte) {
 	mode := m.ctrlReg & (1 << ctrlRegChrBankMode)
 	// 非8KiB模式，切换4KiB的单个chr bank
 	if mode != 0 {
-		offset := m.chrStart + uint16(val&0b11111)*4096
+		offset := m.chrStart + uint32(val&0b11111)*4096
 		m.chrBanks[bank] = m.raw[offset : offset+4096]
-	} else {
+	} else if bank == 0 {
 		// 8KiB模式，切换两个chr banks, 忽略bit0
-		offset := m.chrStart + uint16(val>>1)*8192
+		offset := m.chrStart + uint32(val&0xfe)*4096
 		m.chrBanks[0] = m.raw[offset : offset+4096]
 		m.chrBanks[1] = m.raw[offset+4096 : offset+8192]
 	}
@@ -171,18 +207,22 @@ func (m *Mapper1) writeChrBankReg(bank byte, val byte) {
 
 func (m *Mapper1) writePrgBankReg(val byte) {
 	m.prgBankReg = val
-	mode := m.ctrlReg & (0b11 << ctrlRegPrgBankMode)
-	if mode == 0 || mode == 1 {
-
-		return
+	mode := (m.ctrlReg & (0b11 << ctrlRegPrgBankMode)) >> ctrlRegPrgBankMode
+	offset := m.prgStart + uint32(val&0b1111)*0x4000
+	switch mode {
+	case 0:
+		fallthrough
+	case 1:
+		start := m.prgStart + uint32(val&0xfe)*0x4000
+		m.prgROM[0] = m.raw[start : start+0x4000]
+		m.prgROM[1] = m.raw[start+0x4000 : start+0x8000]
+	case 2:
+		m.prgROM[1] = m.raw[offset : offset+0x4000]
+		m.prgROM[0] = m.raw[m.prgStart : m.prgStart+0x4000]
+	case 3:
+		m.prgROM[0] = m.raw[offset : offset+0x4000]
+		lastPage := m.chrStart - 0x4000
+		m.prgROM[1] = m.raw[lastPage : lastPage+0x4000]
+	default:
 	}
-	var switchIdx byte
-	if mode == 2 {
-		switchIdx = 1
-	} else {
-		switchIdx = 0
-	}
-	// switch 16KiB bank
-	offset := m.prgStart + uint16(val&0xff)*0x4000
-	m.prgROM[switchIdx] = m.raw[offset : offset+0x4000]
 }
