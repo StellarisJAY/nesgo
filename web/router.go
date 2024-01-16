7147c5002e481e31a811addf202c8e87ab97cd07
xxjay <760409558@qq.com>
2024-01-16 10:34:35 +0800

    remove GameService

--- web/router.go
+++ web/router.go
@@ -2,21 +2,17 @@ package web
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/stellarisJAY/nesgo/config"
 	"github.com/stellarisJAY/nesgo/web/middleware"
 	"github.com/stellarisJAY/nesgo/web/service"
 	"net/http"
 )
 
 var (
-	gameService *service.GameService
 	userService = service.NewUserService()
 	roomService = service.NewRoomService()
 )
 
 func setupRouter() *gin.Engine {
-	conf := config.GetEmulatorConfig()
-	gameService = service.NewGameService(conf)
 	r := gin.Default()
 	r.Use(gin.Recovery())
 	r.LoadHTMLGlob("web/ui/*.html")
@@ -28,21 +24,12 @@ func setupRouter() *gin.Engine {
 	r.GET("/room/:roomId", func(c *gin.Context) {
 		c.HTML(200, "room.html", gin.H{})
 	})
-	r.GET("/game/:name", gameService.HandleGamePage)
 
 	// javascript files
 	r.StaticFS("/assets", http.Dir("web/ui/assets"))
 	r.StaticFS("/scripts", http.Dir("web/ui/scripts"))
 
 	authorized := r.Group("/", middleware.ParseQueryToken, middleware.JWTAuth)
-	// game session websocket
-	authorized.GET("/ws/:id", gameService.HandleWebsocket)
-	// boost game session
-	authorized.POST("/game/:id/boost/:rate", gameService.HandleCPUBoost)
-	authorized.POST("/game/:id/pause", gameService.HandlePauseGame)
-	// pause & resume
-	authorized.POST("/game/:id/resume", gameService.HandleResumeGame)
-	authorized.POST("/game/:id/reverse", gameService.HandleReverseOne)
 
 	authorized.POST("/room", roomService.CreateRoom)
 	authorized.GET("/room/list", roomService.ListOwningRooms)
