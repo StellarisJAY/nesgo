68f0bd5e0e86c706b5783338dccdcb1792bd78ee
xxjay <760409558@qq.com>
2024-02-06 10:06:45 +0800

    feat: encoder interface

--- web/codec/encoder.go
+++ web/codec/encoder.go
@@ -1,1 +1,21 @@
 package codec
+
+import (
+	"errors"
+	"github.com/stellarisJAY/nesgo/ppu"
+	"github.com/stellarisJAY/nesgo/web/codec/h264"
+)
+
+type IEncoder interface {
+	Encode(frame *ppu.Frame) error
+	Flush() error
+	FlushBuffer() []byte
+}
+
+func NewEncoder(codec string) (IEncoder, error) {
+	switch codec {
+	case "h264":
+		return h264.NewEncoder()
+	}
+	panic(errors.New("codec not available"))
+}
