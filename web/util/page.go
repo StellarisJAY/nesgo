90727140a0cf5f0c0527329db490b0bcba8014b8
xxjay <760409558@qq.com>
2024-02-18 14:59:46 +0800

    feat: paged query

--- web/util/page.go
+++ web/util/page.go
@@ -1,1 +1,9 @@
 package util
+
+import "gorm.io/gorm"
+
+func Page(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Offset((page - 1) * pageSize).Limit(pageSize)
+	}
+}
