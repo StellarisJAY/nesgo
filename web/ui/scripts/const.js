fa286e32fa9fa229b85ba4a5ea64ac6bc59f8d4f
xxjay <760409558@qq.com>
2024-03-08 13:28:07 +0800

    chore: 删除旧UI

--- web/ui/scripts/const.js
+++
@@ -1,6 +0,0 @@
-const serverAddr = "192.168.0.107:8080"
-const apiServer = "http://" + serverAddr + "/api"
-const websocketAddr = "ws://" + serverAddr + "/ws"
-
-const stunServer = "stun:192.168.0.107:3478"
-const roles = ["Host", "Gamer", "Observer"]
